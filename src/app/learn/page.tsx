bcdee9fde99c97bb22315b99365d05f2e41976b1
finnelliott <finn@finnelliott.com>
2023-05-23 10:10:22 +0100

    First fix, added relevant auth to learn and create sections. Left homepage and all course pages unaffected

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,5 +1,7 @@
+import Header from "@/components/Header";
+
 export default function Page() {
     return (
-        <div className="font-bold text-4xl mx-auto">Learn</div>
+        <div className="font-bold text-4xl mx-auto"><Header />Learn</div>
     )
 }
