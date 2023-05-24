c7d3d21c353f83a27ff273f44e79ad2112c9ad8f
finnelliott <finn@finnelliott.com>
2023-05-24 09:56:58 +0100

    --

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -6,32 +6,6 @@ import { redirect } from "next/navigation";
 export default async function Page() {
     const authenticatedUser = await currentUser();
     if (!authenticatedUser) return <div>Not authenticated</div>
-    
-    const courses = await prisma?.course.findMany({
-        where: {
-            created_by: {
-                clerk_id: authenticatedUser.id
-            }
-        }
-    })
-
-    async function createCourse() {
-        "use server";
-        const authenticatedUser = await currentUser();
-        if (!authenticatedUser) return <div>Not authenticated</div>
-
-        const course = await prisma.course.create({
-            data: {
-                created_by: {
-                    connect: {
-                        clerk_id: authenticatedUser.id
-                    }
-                }
-            }
-        })
-
-        return redirect(`/courses/${course.id}`)
-    }
 
     return (
         <div className="">
